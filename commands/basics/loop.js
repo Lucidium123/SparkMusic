5e2d005250b06ba48c36f5441b039d5496a0ce1f
Lucidium123 <70474631+Lucidium123@users.noreply.github.com>
2021-02-14 07:29:27 -0700

    Update loop.js

--- commands/basics/loop.js
+++ commands/basics/loop.js
@@ -1,9 +1,27 @@
 module.exports = ({
- name: "loop",
- code: `
- $title[Loop]
- $thumbnail[$userAvatar[$authorID]]
- $color[f1f0f0]
- $description[$replaceText[$replaceText[$checkCondition[$loopQueue==true];true;Loop Enabled!];false;Loop disabled!]]
- $onlyIf[$voiceID!=;You are not in a VC!]`
- });
+    name: "loop",
+    aliases: ['repeat'],
+    code: `
+    $elseif[$message[1]==song]
+    $loopSong
+    $title[Loop Manager]
+    $description[Now looping the current song playing!]
+    $author[Lucid]
+    $color[#E5E5E5]
+    
+    $elseIf[$message[1]==queue]
+    $loopQueue
+    $title[Loop Manager]
+    $description[Now looping the queue!]
+    $author[Lucid;https://cdn.discordapp.com/avatars/586391264350699551/78f1439167e0495373e4ab579e16910a.webp?size=128]
+    $color[#E5E5E5]
+    $elseIf[$message[1]==stop]
+    $onlyIfMessageContains[$loopStatus;song;queue;Loop isn't currently active!]
+    $title[Loop Manager]
+    $description[Sucessfuly paused the loop status!]
+    $endelseIf
+    $endelseif
+    $argsCheck[>1;Please provide something to loop (song or queue)!]
+    
+    `
+    })
