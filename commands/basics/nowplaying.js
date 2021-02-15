b8799c7e2e516e9f52482ffae30f35b357025838
Lucidium123 <70474631+Lucidium123@users.noreply.github.com>
2021-02-16 06:40:37 +0700

    Update nowplaying.js

--- commands/basics/nowplaying.js
+++ commands/basics/nowplaying.js
@@ -1,7 +1,16 @@
 module.exports = ({
   name: "nowplaying",
+  channel: "$channelID",
   code: `
-$channelSendMessage[$channelID;{color:RANDOM}{description: Now Playing $songInfo[title] - $songInfo[duration]s}]
+$title[SongInfo (NP)]
+$description[
+Currently Playing: $songInfo[title]
+Song Description: $songInfo[description]
+Duration: $songInfo[duration]
+Requested By: $songInfo[userID]
+Artist: $songInfo[publisher]
+Link: $songInfo[publisher_url]
+$songInfo[thumbnail]]
 $onlyIf[$voiceID!=;{color:RED}{description: Please join a vc first}]
 $onlyIf[$queueLength>0;{color:RED}{description: Queue is empty}]`
 })
