a18084531eff1860ec78a654f31318f2ae68c2bb
Lucidium123 <70474631+Lucidium123@users.noreply.github.com>
2021-02-19 10:20:26 +0800

    Update help.js

--- commands/basics/help.js
+++ commands/basics/help.js
@@ -1,6 +1,6 @@
 module.exports = ({
   name: "help",
   code: $title[Help] $addField[Fun:; \`anime\`, \`avatar\`, \`invite\`, \`meme\`, \`ping\`, \`tts\`]
-  $addField[Mod:; \`checkwarns\`, \`warn\`, \`removewarn\`, \`setprefix\`, \`ban\`, \`kick\`, \`starboard(name of channel dont mention it)\`, \`setreqstar(starcount)\`, \`setchatbot(having bugs at the moment)\`]
+  $addField[Mod:; \`checkwarns\`, \`warn\`, \`removewarn\`, \`setprefix\`, \`ban\`, \`kick\`, \`starboard(name of channel dont mention it)\`, \`setreqstar(starcount)\`]
   $addField[Music:; \`play\`, \`stop\`, \`queue\`, \`pause\`, \`nowplaying\`, \`resume\`, \`loop\`]`
 })
